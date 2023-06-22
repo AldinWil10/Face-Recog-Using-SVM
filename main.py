476a7c40e60ab0a55b0e29dc57b33e0cdd65c56b
AldinWil10 <92682690+AldinWil10@users.noreply.github.com>
2023-06-22 11:51:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import tensorflow as tf
 from sklearn.preprocessing import LabelEncoder
 import pickle
 from keras_facenet import FaceNet
+
 #INITIALIZE
 facenet = FaceNet()
 faces_embeddings = np.load("faces_embeddings_done_4classes.npz")
@@ -18,27 +19,34 @@ haarcascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
 model = pickle.load(open("svm_model_160x160.pkl", 'rb'))
 
 cap = cv.VideoCapture(1)
-# WHILE LOOP
 
+# WHILE LOOP
 while cap.isOpened():
     _, frame = cap.read()
     rgb_img = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
     gray_img = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     faces = haarcascade.detectMultiScale(gray_img, 1.3, 5)
-    for x,y,w,h in faces:
+    
+    for x, y, w, h in faces:
         img = rgb_img[y:y+h, x:x+w]
-        img = cv.resize(img, (160,160)) # 1x160x160x3
-        img = np.expand_dims(img,axis=0)
+        img = cv.resize(img, (160, 160)) # 1x160x160x3
+        img = np.expand_dims(img, axis=0)
         ypred = facenet.embeddings(img)
-        face_name = model.predict(ypred)
-        final_name = encoder.inverse_transform(face_name)[0]
-        cv.rectangle(frame, (x,y), (x+w,y+h), (255,0,255), 10)
-        cv.putText(frame, str(final_name), (x,y-10), cv.FONT_HERSHEY_SIMPLEX,
-                   1, (0,0,255), 3, cv.LINE_AA)
+        
+        if ypred > 0.7:
+            face_name = model.predict(ypred)
+            final_name = encoder.inverse_transform(face_name)[0]
+        else:
+            final_name = "Unknown"
+        
+        cv.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 255), 10)
+        cv.putText(frame, str(final_name), (x, y-10), cv.FONT_HERSHEY_SIMPLEX,
+                   1, (0, 0, 255), 3, cv.LINE_AA)
 
     cv.imshow("Face Recognition:", frame)
-    if cv.waitKey(1) & ord('q') ==27:
+    
+    if cv.waitKey(1) & ord('q') == 27:
         break
 
 cap.release()
-cv.destroyAllWindows
+cv.destroyAllWindows()
