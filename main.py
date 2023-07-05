377ba0dddf0693024ffc6a737046c848c8f1459c
AldinWil10 <92682690+AldinWil10@users.noreply.github.com>
2023-07-05 11:13:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-# face recognition part II
 #IMPORT
 import cv2 as cv
 import numpy as np
 import os
+import time
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf
 from sklearn.preprocessing import LabelEncoder
@@ -18,9 +18,15 @@ encoder.fit(Y)
 haarcascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
 model = pickle.load(open("svm_model_160x160.pkl", 'rb'))
 
-cap = cv.VideoCapture(1)
+cap = cv.VideoCapture(0)
+
+cap.set(3, 1280) #640, 1280, 1920
+cap.set(4, 720) #480, 720, 1080
 
 # WHILE LOOP
+frame_count = 0
+start_time = time.time()
+
 while cap.isOpened():
     _, frame = cap.read()
     rgb_img = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
@@ -32,20 +38,28 @@ while cap.isOpened():
         img = cv.resize(img, (160, 160)) # 1x160x160x3
         img = np.expand_dims(img, axis=0)
         ypred = facenet.embeddings(img)
+        distance = model.decision_function(ypred)
+        min_distance = np.min(distance)
         
-        if ypred > 0.7:
-            face_name = model.predict(ypred)
-            final_name = encoder.inverse_transform(face_name)[0]
+        
+        if min_distance > 0.16:
+            final_name = "unknown"
         else:
-            final_name = "Unknown"
+            face_name = model.predict(ypred)
+            final_name = encoder.inverse_transform(face_name)
         
         cv.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 255), 10)
         cv.putText(frame, str(final_name), (x, y-10), cv.FONT_HERSHEY_SIMPLEX,
                    1, (0, 0, 255), 3, cv.LINE_AA)
 
+    frame_count += 1
+    end_time = time.time()
+    fps = frame_count / (end_time - start_time)
+    cv.putText(frame, f"FPS: {fps:.2f}", (10, 50), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3, cv.LINE_AA)
+
     cv.imshow("Face Recognition:", frame)
-    
-    if cv.waitKey(1) & ord('q') == 27:
+
+    if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
